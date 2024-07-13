7aa701af45b9b4d0ad867b3f39680a74405196ed
Kaya-Sem <kayasemvc@gmail.com>
2024-07-13 20:50:33 +0200

    working piechart
    no colors yet however

--- src/collection_view.py
+++ src/collection_view.py
@@ -2,31 +2,24 @@ import term_piechart
 from typing import List, Dict
 from database import get_collection
 
+
 def transform_to_pie_data(categories_weights: Dict[str, float]) -> List[dict]:
     data = []
     for category, weight in categories_weights.items():
-        requests.append({"name": str(category), "value": int(weight)})
+        data.append({"name": f"{category.ljust(10)} {format_weight(int(weight)).ljust(4)}", "value": int(weight)})
     return data
-
-
-data = [
-    {"name": "GET", "value": 9983},
-    {"name": "POST", "value": 7005},
-    {"name": "DELETE", "value": 3323},
-    {"name": "PUT", "value": 2794},
-    {"name": "PATCH", "value": 1711},
-]
 
 
 def get_piechart(data: List[dict]):
     pie = term_piechart.Pie(
         data,
-        radius=5,
+        radius=4,
         autocolor=False,
-        autocolor_pastel_factor=0.7,
-        legend={"line": 0, "format": "{label} {name:<8} {percent:>5.2f}% [{value}]"},
+        autocolor_pastel_factor=0.0,
+        legend={"line": 0, "format": "{percent:>5.2f}% {label} {name:<10} "},
     )
     return pie
 
-#print(get_collection(6).get_category_weights())
-# {'Shelter': 3300.0, 'Sleep': 960.0, 'Clothing': 530.0}
+
+def format_weight(weight):
+    return f"{weight / 1000:.1f} kg" if weight >= 1000 else f"{int(weight)} g"
